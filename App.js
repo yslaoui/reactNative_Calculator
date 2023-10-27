8b935b14371e5e4d1a0b5eebc48fc3b46c093380
yslaoui <yslaouiyassine@gmail.com>
2023-10-27 16:53:36 +0100

    creating grid of buttons

--- App.js
+++ App.js
@@ -1,13 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions, SafeAreaView, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
+const screenWidth = Dimensions.get('window').width;
 
 export default function App() {
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.container}>
-        <Text style={styles.result}>32</Text>
+        <Text style={styles.result}>34</Text>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={[styles.button, styles.toprow]}>
+            <Text style={styles.toprowtext}> C </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.toprow]}>
+            <Text style={styles.toprowtext}> +/- </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.toprow]}>
+            <Text style={styles.toprowtext}> % </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.siderow]}>
+            <Text style={styles.action}> / </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 7 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 8 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 9 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.siderow]}>
+            <Text style={styles.action}> x </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 4 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 5 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 6 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.siderow]}>
+            <Text style={styles.action}> - </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 1 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 2 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> 3 </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.siderow]}>
+            <Text style={styles.action}> + </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> C </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> +/- </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.action}> . </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.siderow]}>
+            <Text style={styles.action}> = </Text>
+          </TouchableOpacity>
+        </View>
+
+
       </View>
       </SafeAreaView>
   );
@@ -17,13 +96,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    justifyContent: 'flex-end', // all child components aligned to the center, vertically
+    justifyContent: 'flex-end'
   },
   result: 
   {    
-    color: "white", 
+    color: 'white', 
     fontSize: 50,
-    textAlign: "right", // Align horizontally, 
+    textAlign: 'right', // Align horizontally, 
+  },
+  row: 
+  {
+    flexDirection: 'row', 
+    marginBottom: '3%'
+  }, 
+  button:
+  {
+    backgroundColor: '#595959', 
+    width: screenWidth/5, 
+    height: screenWidth/5, 
+    marginLeft: "5%",
+    marginright: "5%",
+    margintop: "10%",
+    marginbottom: "10%",
+    borderRadius: screenWidth/10, // should be half the width and height for the button to appear circullar
+    justifyContent: 'center'
+  }, 
+  action: 
+  {
+    color: 'white', 
+    fontSize: screenWidth/15, 
+    textAlign: 'center', 
+  },
+  toprow:  
+  {
+    backgroundColor: '#a6a6a6'
+  },
+  toprowtext: 
+  {
+    color: 'black', 
+    fontSize: screenWidth/15, 
+    textAlign: 'center', 
+  },
+  siderow:  
+  {
+    backgroundColor: '#3399ff'
   }
-
 });
