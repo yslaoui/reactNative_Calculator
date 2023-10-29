a41daad22152a4121214f28c0a0167fe5847913d
yslaoui <yslaouiyassine@gmail.com>
2023-10-29 21:57:29 +0100

    Adding result state and alerts when pressing the buttons

--- App.js
+++ App.js
@@ -3,85 +3,85 @@ import { StyleSheet, Text, View, Dimensions, SafeAreaView, TouchableOpacity } fr
 import React, { useState } from 'react';
 const screenWidth = Dimensions.get('window').width;
 
+
 export default function App() {
+
+  const [answerValue, setAnswerValue] = useState(0);
+  const buttonPressed = (value) => alert(`Hi ${value}`);
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.container}>
-        <Text style={styles.result}>34</Text>
-
+        <Text style={styles.result}>{answerValue}</Text>
         <View style={styles.row}>
-          <TouchableOpacity style={[styles.button, styles.toprow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('C'))} style={[styles.button, styles.toprow]}>
             <Text style={styles.toprowtext}> C </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.toprow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('+/-'))} style={[styles.button, styles.toprow]}>
             <Text style={styles.toprowtext}> +/- </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.toprow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('%'))} style={[styles.button, styles.toprow]}>
             <Text style={styles.toprowtext}> % </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.siderow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('/'))} style={[styles.button, styles.siderow]}>
             <Text style={styles.action}> / </Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('7'))} style={styles.button}>
             <Text style={styles.action}> 7 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('8'))} style={styles.button}>
             <Text style={styles.action}> 8 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('9'))} style={styles.button}>
             <Text style={styles.action}> 9 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.siderow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('x'))} style={[styles.button, styles.siderow]}>
             <Text style={styles.action}> x </Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('4'))} style={styles.button}>
             <Text style={styles.action}> 4 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('5'))} style={styles.button}>
             <Text style={styles.action}> 5 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('6'))} style={styles.button}>
             <Text style={styles.action}> 6 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.siderow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('-'))} style={[styles.button, styles.siderow]}>
             <Text style={styles.action}> - </Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('1'))} style={styles.button}>
             <Text style={styles.action}> 1 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('2'))} style={styles.button}>
             <Text style={styles.action}> 2 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('3'))} style={styles.button}>
             <Text style={styles.action}> 3 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.siderow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('+'))} style={[styles.button, styles.siderow]}>
             <Text style={styles.action}> + </Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.action}> C </Text>
+          <TouchableOpacity onPress={()=>buttonPressed(('0'))} style={[styles.button, {width: screenWidth/2.25}]}>
+            <Text style={styles.action}> 0 </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.action}> +/- </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={()=>buttonPressed(('.'))} style={styles.button}>
             <Text style={styles.action}> . </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={[styles.button, styles.siderow]}>
+          <TouchableOpacity onPress={()=>buttonPressed(('='))} style={[styles.button, styles.siderow]}>
             <Text style={styles.action}> = </Text>
           </TouchableOpacity>
         </View>
